550c80f273fa7d20f99c6e8357c095995d6c7c69
sharonnkatha <59752338+sharonnkatha@users.noreply.github.com>
2020-01-24 17:55:42 -0700

    dsr

---
+++ Decisionsolutions/Decisionsolutions/highmedlow/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace highmedlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 9;
+            int num3 = 6;
+            int high, mid, low;
+            //assume one of the numbers is the highest
+            high = num1;
+            if (num2 > high)
+            {
+                high = num2; //new highest number
+                if (num3 > high)
+                {
+                    high = num3;
+                    mid = num2;
+                    low = num1;
+
+                }// goes to Writeline at end of nested if
+                else
+                {
+                    if ( num3 > num1)
+                    {
+                        mid = num3;
+                        low = num1;
+
+                    }//goes to Writeline at end of nested if
+                    else
+                    {
+                        mid = num1;
+                        low = num3;
+
+                    }
+                    if (num3> high)
+                    {
+                        high = num3;
+                        mid = num1;
+                        low = num2;
+
+
+                    }// goes to Writeline at end of nested if
+                    else
+                        if (num3 > num2 )
+                    {
+                        mid = num3;
+                        low = num2;
+                    }
+                    else
+                    {
+                        mid = num2;
+                        low = num3;
+                    }//goes to Writeline at end of nested if
+
+                }//end of the nested if
+                Console.WriteLine($"num1 {num1} num2 {num2} num3 {num3} order is high {high} mid {mid} low {low}");
+                Console.ReadKey();
+
+            }
+           
+            
+
+
+            
+
+
+        }
+    }
+}
---
+++ Decisionsolutions/highmidlow/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace highmidlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//to save time in the demo,the test values will be hardcoded in
+        }
+    }
+}
