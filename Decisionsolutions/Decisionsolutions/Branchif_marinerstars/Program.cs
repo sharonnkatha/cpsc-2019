868ab32622481b3ced54919a7608b5ca241309e3
sharonnkatha <59752338+sharonnkatha@users.noreply.github.com>
2020-01-27 18:43:00 -0700

    gu

---
+++ Decisionsolutions/Decisionsolutions/Branchif_marinerstars/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branchif_marinerstars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int StarLevel = 3;
+            string msg = "";   //empty messsage string
+
+            if (StarLevel < 1)  
+            {
+                msg = ("Welcome to Holland America Cruises.You have yet to earn speecial benefits");
+            }
+            else if (StarLevel<2)
+            {
+                msg = "You can receive any of the 1-star benefits of the mariner program";
+            }
+            else if (StarLevel<3)
+            {
+                msg = "You can receive any of the 2-star benefits of the mariner program";
+            }
+            else if (StarLevel<4)
+            {
+                msg = "You can receive any of the 3-star benefits of the mariner program";
+            }
+            else if (StarLevel<5)
+            {
+                msg = "You can receive any of the 4-star benefits of the mariner program";
+            }
+            else
+            {
+                msg = "You can receive any of the 5-star benefits of the mariner program";
+               
+            }
+            Console.WriteLine(msg);
+        }
+    }
+}
---
+++ Decisionsolutions/Decisionsolutions/logicalconditions/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logicalconditions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //logical conditions are two or more conditions
+            //separated by a logic expression
+            //relative operator
+            //> >= < <= == !=
+            //operan operator operan
+            //high>low
+            //logica operators
+            //|| (or) && (and) ! (not) 
+
+            int Age = 6;
+            if (Age< 4)
+            {
+                Console.WriteLine("Too young");
+                
+            }
+            else if (Age > 14)
+            {
+                Console.WriteLine("Too old");
+
+            }
+            else
+            {
+                Console.WriteLine("Welcome to the league");
+
+            }
+            //but what about using logical operators
+            //note operand is repeated for each relative test condition
+            //(Age < 4 || Age >14) is correct syntax
+
+            //Truth table
+            //condition     or        and
+            // T T          T           T
+            // T F          T           F
+            // F T          T           F
+            // F F          F           F
+
+            //SUMMARY && only true if Both conditions are true
+            //        || only false if Both conditions are false
+
+            if (Age < 4 || Age > 14)
+            {
+                //one does not know if they are too old or too young
+                Console.WriteLine("Sorry you are not in the age bracket for the league");
+               
+            }
+            else
+            {
+                Console.WriteLine("Welcome to the league");
+            }
+        }
+    }
+}
