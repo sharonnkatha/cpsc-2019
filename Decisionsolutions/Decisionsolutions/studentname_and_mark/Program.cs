f20d43e8d8b78c9bb5f1cb51cc732662a5a05c24
sharonnkatha <59752338+sharonnkatha@users.noreply.github.com>
2020-01-30 10:45:29 -0700

    nj

---
+++ Decisionsolutions/Decisionsolutions/studentname_and_mark/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentname_and_mark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Grade;
+            double StudentMark;
+            string InputValue;
+            string StudentName;
+
+            Console.WriteLine("Please enter your name ");
+            InputValue = Console.ReadLine();
+            StudentName = InputValue;
+
+            Console.WriteLine("Please enter your Mark ");
+            InputValue = Console.ReadLine();
+            StudentMark = double.Parse(InputValue);
+            
+            if (StudentMark =)
+                
+        }
+    }
+}
---
+++ Decisionsolutions/Decisionsolutions/studentname_and_mark/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("studentname_and_mark")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("studentname_and_mark")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4abf52cb-44c5-4c18-9f46-668de315a16d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
