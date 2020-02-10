d17beac908b7198cf6fbafb6b77cfa5575e32645
sharonnkatha <59752338+sharonnkatha@users.noreply.github.com>
2020-02-10 16:17:25 -0700

    switch

---
+++ Decisionsolutions/Decisionsolutions/switchpractice/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switchpractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*program to calculate how much to bill customers
+             *A. 9.95 per month upto 10hrs additional hrs $2.00 per hr
+             *B.13.95 "          "   20hrs                $1.00
+             *C. 19.95           for unlimited hrs
+             * 
+             */
+            string packageLetter;
+            int numOfHours;
+            string inputValue;
+            double totalPayment=0;
+            
+            Console.WriteLine("Enter the service package A,B or C");
+            packageLetter = Console.ReadLine();
+             
+
+            Console.WriteLine("Please enter the number of hours");
+            inputValue = Console.ReadLine();
+            numOfHours = int.Parse(inputValue);
+
+            switch(packageLetter.ToUpper())
+            {
+                case "A":
+                    {
+                        Console.WriteLine("The plan is 9.95 per month upto 10hrs additional hrs = $2.00 per hr");
+                        totalPayment = 9.95 + ((numOfHours - 10) * 2);
+                        break;
+                    }
+                case "B":
+                    {
+                        Console.WriteLine("13.95 per month upto 20hrs additional hrs= $1.00 per hr");
+                        totalPayment = 13.95 + (numOfHours - 20) ;
+                        break;
+                    }
+                case "C":
+                    {
+                        Console.WriteLine("19.95 for unlimited hrs");
+                        totalPayment = 19.95;
+                        break;
+                    }
+                    
+            }
+            Console.WriteLine($"The amount due on your {packageLetter} plan is ${totalPayment}" );
+
+
+        }
+    }
+}
---
+++ Decisionsolutions/Decisionsolutions/unitconversionsprogram/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unitconversionsprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //question 2
+            /*
+             * program for simple unit conversions
+             * display menu of availabel options
+             * prompt for desired option 
+             * prompt for a value
+             * conversion should be performed and the new value should be applied
+             * us e thye table 
+             * 
+             * allow for conversions in the opposite direction
+             * eg kg to pounds and vice versa
+             * ikg = 2.2
+             * CONVERSION FORMULA
+                POUNDS TO KILOGRAMS             kg = lb / 2.2046
+                FLUID OUNCES (U.S.) TO LITERS   1 liter = 33.8140226 ounces
+               INCHES TO CENTIMETERS            cm = in / 0.39370
+                 FAHRENHEIT TO CELSIUS          C = (F - 32) / 1.800
+             */
+            
+            string conversionOption="";
+            double Value;
+            double Total = 0;
+
+            //menu of available options
+            Console.WriteLine("Unit of Conversions Program");
+            Console.WriteLine("Choose between these options");
+            Console.WriteLine("1a.Pounds to kilograms");
+            Console.WriteLine("1b.kilograms to pounds");
+            Console.WriteLine("2a.fluid ounces to litres");
+            Console.WriteLine("2b.litres to ounces");
+            Console.WriteLine("3a.inches to centimeters");
+            Console.WriteLine("3b.centimetres to inches");
+            Console.WriteLine("4a.fahrenheit to celcius");
+            Console.WriteLine("4b.celcius to farenheit\n");
+
+            Console.WriteLine("Enter the desired unit of conversion ");
+            string inputValue=Console.ReadLine();
+            conversionOption = inputValue;
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Enter the value of unit");
+            inputValue = Console.ReadLine();
+            Value = double.Parse(inputValue);
+
+
+            
+            switch (conversionOption.ToLower())
+            {
+                case "a":
+                    {
+                        Console.WriteLine("Pounds to kilograms Unit Conversion");
+                        Total = (Value / 2.2046);
+                       
+                        break;
+                    }
+                case "b":
+                    {
+                        Console.WriteLine("kilograms to pounds Unit Conversion");
+                        Total = (Value * 2.2046);
+                     
+                        break;
+                    }
+                case "c":
+                    {
+                        Console.WriteLine(" ounces to litres Unit Conversion");
+                        Total = (Value * 33.8140226);
+                        break;
+                    }
+                case "d":
+                    {
+                        Console.WriteLine(" litres to ounces Unit Conversion");
+                        Total = (Value / 33.8140226);
+                        break;
+                    }
+                case "e":
+                    {
+                        Console.WriteLine("inches centimeters Unit Conversion");
+                        Total = ( Value / 0.39370);
+                        break;
+                    }
+                case "f":
+                    {
+                        Console.WriteLine("centimeters to Unit Conversion");
+                        Total = (Value * 0.39370);
+                        break;
+                    }
+                case "g":
+                    {
+                        Console.WriteLine("fahrenheit to celcius Unit Conversion");
+                        Total = ((Value - 32) / 1.8);
+                        break;
+                    }
+                case "h":
+                    {
+                        Console.WriteLine("celcius to fahrenheit  Unit Conversion");
+                        Total = ((Value * 1.8) + 32);
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("The letter entered is invalid");
+                        break;
+                    }
+
+            }
+            Console.WriteLine($"Answer=\t {Total}" );
+
+
+            
+        }
+    }
+}
